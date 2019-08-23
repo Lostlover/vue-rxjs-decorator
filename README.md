5ba91f70b03739145a179611076fdfc173bf0a6e
Lostlover <497278979@qq.com>
2019-08-23 14:22:42 +0800

    rewrite

--- README.md
+++ README.md
@@ -9,31 +9,97 @@ npm i -S vue-rxjs-decorator
 ```
 
 ## Usage
-`@ObservableData(key: string , options: ObservableDataOptions = {share: false})`
 
+First, you should use VueRx plugin;
+
+```typescript
+// main.ts
+import VueRx from "vue-rx";
+
+Vue.use(VueRx);
+```
+
+`@ObservableData(name: string)`
 
 ```html
 <template>
   <div>{{data$}}</div>
 </template>
 ```
+
 ```typescript
-import {ObservableData} from "vue-rxjs-decorator"
+import { ObservableData } from "vue-rxjs-decorator";
 
 @Component
 export default class App extends Vue {
-    data$!:any;
-    
-    @ObservableData("data$")
-    _data$() {
-        return api$({}).pipe(this.handle$())
-    }
+  @ObservableData("data$")
+  _data$() {
+    return observable.pipe(handle);
+  }
+
+  created() {
+    // you can subscribe like this
+    this.$observables.data$.subscribe();
+  }
+}
+```
+
+`@EventSubject()`
+
+```html
+<template>
+  <button v-stream:click="{subject: streamClick, data: 1}">add</button>
+  <div>{{data$}}</div>
+</template>
+```
+
+```typescript
+import { ObservableData, EventSubject } from "vue-rxjs-decorator";
+
+@Component
+export default class App extends Vue {
+  data$!: any;
+
+  @EventSubject()
+  streamClick!: Subject<any>;
+
+  @ObservableData("data$")
+  _data$() {
+    return this.streamClick.pipe(handle);
+  }
+}
+```
+
+`@ObservableMethod(name: string)`
+
+```html
+<template>
+  <div>{{data$}}</div>
+  <button @click="addHandle(100)"></button>
+</template>
+```
+
+```typescript
+import { ObservableData, ObservableMethod } from "vue-rxjs-decorator";
+
+@Component
+export default class App extends Vue {
+  addHandle!: (n: number) => void;
+
+  @ObservableData("data$")
+  _data$() {
+    return this.addHandle$.pipe(handle);
+  }
+
+  @ObservableMethod("addHandle")
+  addHandle$!: Observable<number>;
 }
 ```
 
 ## See also
 
-[vue-property-decorator](https://github.com/kaorun343/vue-property-decorator)
+[vue-rx](https://github.com/vuejs/vue-rx)
+[vue-class-component](https://github.com/vuejs/vue-class-component)
 
 ## License
 
--- src/decorator.ts
+++ src/decorator.ts
@@ -1,47 +1,35 @@
-import { Subscription } from "rxjs";
-import { defineReactive } from "./util";
 import { createDecorator } from "vue-class-component";
 
-export const subscriptionSymbol = Symbol("subscription");
+export function ObservableData(name: string) {
+  return createDecorator(function(componentOptions: any, key: string) {
+    const mixin = {
+      subscriptions() {
+        return {
+          [name]: componentOptions["methods"][key].call(this)
+        };
+      }
+    };
+    const mixins = componentOptions.mixins || [];
+    mixins.push(mixin);
+  });
+}
 
-export function ObservableData<T>(key: string) {
-  return function(this: any, target: any, name: string, describer: any) {
-    const curKey = key || `${name}$`;
-    let observable: any;
+export function EventSubject() {
+  return createDecorator(function(componentOptions: any, key, index) {
+    const mixin = {
+      domStreams: [key]
+    };
+    const mixins = componentOptions.mixins || [];
+    mixins.push(mixin);
+  });
+}
 
-    createDecorator(function(this: any, componentOptions: any, key, index) {
-      const mixins = {
-        created(this: any) {
-          const _this = this;
-
-          observable = componentOptions["methods"][key].call(_this);
-
-          defineReactive(_this, curKey, undefined);
-
-          const s = observable.subscribe(
-            (value: any) => {
-              _this[curKey] = value;
-              _this.$forceUpdate();
-            },
-            (error: any) => {
-              throw error;
-            }
-          );
-
-          if (!this[subscriptionSymbol]) {
-            this[subscriptionSymbol] = new Subscription();
-          }
-          this[subscriptionSymbol].add(s);
-        },
-        beforeDestroy(this: any) {
-          const _subscriptions: Subscription = this[subscriptionSymbol];
-
-          if (_subscriptions) {
-            _subscriptions.unsubscribe();
-          }
-        }
-      };
-      componentOptions.mixins!.push(mixins);
-    })(target, name);
-  };
+export function ObservableMethod(name: string) {
+  return createDecorator(function(componentOptions: any, key: string) {
+    const mixin = {
+      observableMethods: [name]
+    };
+    const mixins = componentOptions.mixins || [];
+    mixins.push(mixin);
+  });
 }
